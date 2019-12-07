63818f0f6409f084d17d7265db9e23ccd6d26953
sriramez <velayutham.sriram@gmail.com>
2019-12-07 15:01:40 +0530

    added swagger changes

--- src/main/java/com/gbs/app/GbsApplication.java
+++ src/main/java/com/gbs/app/GbsApplication.java
@@ -24,7 +24,7 @@ public class GbsApplication {
 		public Docket productApi() {
 
 			return new Docket(DocumentationType.SWAGGER_2).select()
-					.apis(RequestHandlerSelectors.basePackage("com.gym.app.controllers")).build();
+					.apis(RequestHandlerSelectors.basePackage("com.ggbs.app.controllers")).build();
 		}
 	}
 }
